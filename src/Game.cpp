a79f90a213858a197f1358cc7fc7864bbb32eb27
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2025-05-07 22:40:25 -0400

    Multisampling

--- src/Game.cpp
+++ src/Game.cpp
@@ -21,6 +21,7 @@ void Game::render() {
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
+    glEnable(GL_MULTISAMPLE);
 
     glClearColor(0.05f, 0.01f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
